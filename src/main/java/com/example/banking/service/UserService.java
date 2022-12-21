17a380dce58f783510765c2bfe7c6f8e78afd405
Igor <ig.zuban1ch@gmail.com>
2022-12-21 10:08:08 +0200

    feat: create UserService and add its implementation

---
+++ src/main/java/com/example/banking/service/UserService.java
@@ -0,0 +1,13 @@
+package com.example.banking.service;
+
+import com.example.banking.dto.RegisterRequest;
+
+import java.util.Map;
+
+public interface UserService {
+    void register(RegisterRequest request);
+
+    Map<String, String> verifyUser(String phoneNumber, String password);
+
+    Map<String, String> getTokens(String refreshToken);
+}
