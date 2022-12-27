729d629642d0492c339eceecb5b9e14311a59025
Igor <ig.zuban1ch@gmail.com>
2022-12-27 23:06:01 +0200

    feat: create dto for cards

---
+++ src/main/java/com/example/banking/dto/Card.java
@@ -0,0 +1,18 @@
+package com.example.banking.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@Getter
+public class Card {
+    private int id;
+    private String type;
+    private String currency;
+    private String provider;
+    private BigDecimal sum;
+    private String cardNumber;
+    private String expireDate; // month/year format
+}
