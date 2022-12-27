c88a5ae5698dd167d4dd8a941b89485c7f22125a
Igor <ig.zuban1ch@gmail.com>
2022-12-28 00:14:15 +0200

    feat: add repository for TransactionEntity

---
+++ src/main/java/com/example/banking/repository/TransactionRepository.java
@@ -0,0 +1,14 @@
+package com.example.banking.repository;
+
+import com.example.banking.entity.TransactionEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<TransactionEntity, Integer> {
+    @Query("FROM TransactionEntity WHERE sender.id = :cardId OR receiver.id = :cardId ORDER BY time DESC")
+    List<TransactionEntity> getAllByCardId(Integer cardId);
+}
