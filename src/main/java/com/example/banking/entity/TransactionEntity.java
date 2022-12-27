efa91dbd978239ce87ec7b3f5fa04d0afbdb5f60
Igor <ig.zuban1ch@gmail.com>
2022-12-28 00:13:04 +0200

    feat: create entity for transactions

---
+++ src/main/java/com/example/banking/entity/TransactionEntity.java
@@ -0,0 +1,46 @@
+package com.example.banking.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "transactions")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString(exclude = {"sender", "receiver"})
+public class TransactionEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "sender_card_id")
+    private CardEntity sender;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "receiver_card_id")
+    private CardEntity receiver;
+
+    private String purpose;
+
+    private ZonedDateTime time;
+
+    private BigDecimal sum;
+
+    @Column(name = "converted_sum")
+    private BigDecimal convertedSum;
+
+    private BigDecimal commission;
+
+    @Column(name = "converted_commission")
+    private BigDecimal convertedCommission;
+
+}
