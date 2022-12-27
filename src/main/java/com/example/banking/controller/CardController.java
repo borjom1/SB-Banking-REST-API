0b61b2cce4f37f4321f6e8c6e43e16457f516ef7
Igor <ig.zuban1ch@gmail.com>
2022-12-27 23:58:05 +0200

    feat(CardController): add end-point for creating new card

--- src/main/java/com/example/banking/controller/CardController.java
+++ src/main/java/com/example/banking/controller/CardController.java
@@ -1,19 +1,22 @@
 package com.example.banking.controller;
 
 import com.example.banking.dto.Card;
+import com.example.banking.dto.NewCardRequest;
 import com.example.banking.exception.CardNotFoundException;
+import com.example.banking.exception.CardsLimitException;
 import com.example.banking.security.jwt.JWTUserDetails;
 import com.example.banking.service.CardService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -42,6 +45,21 @@ public class CardController {
             return ResponseEntity.ok(cardService.getCvv(principal.getId(), id));
         } catch (CardNotFoundException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/new")
+    public ResponseEntity<Map<String, String>> createCard(@Valid @RequestBody NewCardRequest request) {
+        Map<String, String> result = new HashMap<>();
+        try {
+            var principal = (JWTUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            log.info("IN CardController -> createCard() for user-id:{}, opts = {}", principal.getId(), request);
+            cardService.createCard(request, principal.getId());
+            result.put("message", "success");
+            return ResponseEntity.ok(result);
+        } catch (BadCredentialsException | CardsLimitException e) {
+            result.put("error", e.getMessage());
+            return ResponseEntity.badRequest().body(result);
         }
     }
 }
