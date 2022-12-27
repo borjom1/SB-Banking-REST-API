4f3b15fd60eaf9943fd44e52b83fa67cf60345f7
Igor <ig.zuban1ch@gmail.com>
2022-12-28 00:16:14 +0200

    feat: add exceptions for transactions

---
+++ src/main/java/com/example/banking/exception/CardCredentialsException.java
@@ -0,0 +1,7 @@
+package com.example.banking.exception;
+
+public class CardCredentialsException extends Exception {
+    public CardCredentialsException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/banking/exception/ViolationPrivacyException.java
@@ -0,0 +1,7 @@
+package com.example.banking.exception;
+
+public class ViolationPrivacyException extends Exception {
+    public ViolationPrivacyException(String message) {
+        super(message);
+    }
+}
