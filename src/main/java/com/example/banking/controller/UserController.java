6af44aaa7a66e23a50de7a6b709ba32ba7a51297
Igor <ig.zuban1ch@gmail.com>
2022-12-30 11:51:36 +0200

    feat: create user controller

---
+++ src/main/java/com/example/banking/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.example.banking.controller;
+
+import com.example.banking.dto.UserInfo;
+import com.example.banking.security.jwt.JWTUserDetails;
+import com.example.banking.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    
+}
