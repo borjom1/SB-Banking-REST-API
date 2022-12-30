74b51166195793346efe76d47bf734d30f44f7cd
Igor <ig.zuban1ch@gmail.com>
2022-12-30 12:08:01 +0200

    feat: create Currency controller

---
+++ src/main/java/com/example/banking/controller/CurrencyController.java
@@ -0,0 +1,24 @@
+package com.example.banking.controller;
+
+import com.example.banking.service.CurrencyService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/currency")
+public class CurrencyController {
+
+    private final CurrencyService currencyService;
+
+    @Autowired
+    public CurrencyController(CurrencyService currencyService) {
+        this.currencyService = currencyService;
+    }
+    
+}
