cdf621e11fa0cfef8c1db68c4e029e8f5fa6830d
Igor <ig.zuban1ch@gmail.com>
2024-01-01 12:15:51 +0200

    feat: add controller details

--- src/main/java/com/example/banking/controller/AuthController.java
+++ src/main/java/com/example/banking/controller/AuthController.java
@@ -5,6 +5,9 @@ import com.example.banking.dto.auth.RefreshRequest;
 import com.example.banking.dto.auth.RegisterRequest;
 import com.example.banking.dto.auth.TokenResponse;
 import com.example.banking.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +15,10 @@ import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.HttpStatus.*;
 
+@Tag(
+        name = "Authorization",
+        description = "User back-end lifecycle (registration, login, logout, etc.)"
+)
 @Slf4j
 @RestController
 @RequestMapping("/auth")
@@ -20,6 +27,7 @@ public class AuthController {
 
     private final UserService userService;
 
+    @Operation(summary = "Register a new user account")
     @ResponseStatus(CREATED)
     @PostMapping("/register")
     public void register(@Valid @RequestBody RegisterRequest body) {
@@ -27,6 +35,7 @@ public class AuthController {
         userService.register(body);
     }
 
+    @Operation(summary = "Log in existing account")
     @ResponseStatus(OK)
     @PatchMapping("/login")
     public TokenResponse login(@Valid @RequestBody LoginRequest body) {
@@ -39,6 +48,10 @@ public class AuthController {
                 .build();
     }
 
+    @Operation(
+            summary = "Refresh access and refresh token",
+            description = "Old refresh token will be unavailable to use"
+    )
     @ResponseStatus(OK)
     @PatchMapping("/refresh")
     public TokenResponse refresh(@Valid @RequestBody RefreshRequest body) {
@@ -51,6 +64,10 @@ public class AuthController {
                 .build();
     }
 
+    @Operation(
+            summary = "Log out from user account",
+            description = "Existing refresh token will be deleted. Access token will be active till expiration date"
+    )
     @ResponseStatus(NO_CONTENT)
     @PatchMapping("/logout")
     public void logout(@Valid @RequestBody RefreshRequest body) {
--- src/main/java/com/example/banking/controller/UserController.java
+++ src/main/java/com/example/banking/controller/UserController.java
@@ -3,6 +3,8 @@ package com.example.banking.controller;
 import com.example.banking.dto.UserInfo;
 import com.example.banking.security.jwt.JWTUserDetails;
 import com.example.banking.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "User account management")
 @Slf4j
 @RestController
 @RequestMapping("/user")
@@ -22,7 +25,7 @@ public class UserController {
         this.userService = userService;
     }
 
-
+    @Operation(summary = "Retrieve user info")
     @GetMapping("/info")
     public UserInfo getInfo() {
         var principal = (JWTUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
